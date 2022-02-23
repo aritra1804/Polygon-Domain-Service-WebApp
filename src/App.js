243fc3628273710d8780ec537bc74ed2089f981e
aritra1804 <aritradutta04@gmail.com>
2022-02-23 14:30:43 +0530

    Mint data added💳

--- src/App.js
+++ src/App.js
@@ -32,55 +32,55 @@ const App = () => {
       console.log(error)
     }
   }
+  const mintDomain = async () => {
+	// Don't run if the domain is empty
+	if (!domain) { return }
+	// Alert the user if the domain is too short
+	if (domain.length < 3) {
+		alert('Domain must be at least 3 characters long');
+		return;
+	}
+	// Calculate price based on length of domain (change this to match your contract)	
+	// 3 chars = 0.5 MATIC, 4 chars = 0.3 MATIC, 5 or more = 0.1 MATIC
+	const price = domain.length === 3 ? '0.5' : domain.length === 4 ? '0.3' : '0.1';
+	console.log("Minting domain", domain, "with price", price);
+  try {
+	const { ethereum } = window;
+	if (ethereum) {
+	  const provider = new ethers.providers.Web3Provider(ethereum);
+	  const signer = provider.getSigner();
+	  const contract = new ethers.Contract(CONTRACT_ADDRESS, contractABI.abi, signer);
 
+			console.log("Going to pop wallet now to pay gas...")
+	  let tx = await contract.register(domain, {value: ethers.utils.parseEther(price)});
+	  // Wait for the transaction to be mined
+			const receipt = await tx.wait();
+
+			// Check if the transaction was successfully completed
+			if (receipt.status === 1) {
+				console.log("Domain minted! https://mumbai.polygonscan.com/tx/"+tx.hash);
+				
+				// Set the record for the domain
+				tx = contract.setRecord(domain, record);
+				await tx.wait();
+
+				console.log("Record set! https://mumbai.polygonscan.com/tx/"+tx.hash);
+				
+				setRecord('');
+				setDomain('');
+			}
+			else {
+				alert("Transaction failed! Please try again");
+			}
+	}
+  }
+  catch(error){
+	console.log(error);
+  }
+}
 	const checkIfWalletIsConnected = async () => {
-		const mintDomain = async () => {
-			// Don't run if the domain is empty
-			if (!domain) { return }
-			// Alert the user if the domain is too short
-			if (domain.length < 3) {
-				alert('Domain must be at least 3 characters long');
-				return;
-			}
-			// Calculate price based on length of domain (change this to match your contract)	
-			// 3 chars = 0.5 MATIC, 4 chars = 0.3 MATIC, 5 or more = 0.1 MATIC
-			const price = domain.length === 3 ? '0.5' : domain.length === 4 ? '0.3' : '0.1';
-			console.log("Minting domain", domain, "with price", price);
-		  try {
-			const { ethereum } = window;
-			if (ethereum) {
-			  const provider = new ethers.providers.Web3Provider(ethereum);
-			  const signer = provider.getSigner();
-			  const contract = new ethers.Contract(CONTRACT_ADDRESS, contractABI.abi, signer);
 		
-					console.log("Going to pop wallet now to pay gas...")
-			  let tx = await contract.register(domain, {value: ethers.utils.parseEther(price)});
-			  // Wait for the transaction to be mined
-					const receipt = await tx.wait();
-		
-					// Check if the transaction was successfully completed
-					if (receipt.status === 1) {
-						console.log("Domain minted! https://mumbai.polygonscan.com/tx/"+tx.hash);
-						
-						// Set the record for the domain
-						tx = contract.setRecord(domain, record);
-						await tx.wait();
-		
-						console.log("Record set! https://mumbai.polygonscan.com/tx/"+tx.hash);
-						
-						setRecord('');
-						setDomain('');
-					}
-					else {
-						alert("Transaction failed! Please try again");
-					}
-			}
-		  }
-		  catch(error){
-			console.log(error);
-		  }
-		}
-	};
+	 };
 
 	// Render methods
 	const renderNotConnectedContainer = () => (
